0be781f0d71667904ee373e6b90e31dde073d863
Tridev9 <triddev123@gmail.com>
2024-08-02 19:05:16 +0530

    Final tri done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
           <LinkContainer to='/'>
             <Navbar.Brand >
               <img src={logo} alt="ProShop" />
-              ProShop
+              TriShop
               </Navbar.Brand> 
           </LinkContainer>    
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
